e0a465e0791e62ded388800e25897414af8f548b
Maciej Chlebny <chlebny59@gmail.com>
2018-11-30 23:27:35 +0100

    Field size changing

--- SaperUnix/Program.cs
+++ SaperUnix/Program.cs
@@ -13,27 +13,27 @@ namespace SaperUnix
 
         class Program
         {
-            static void MinesSet(Pole[,] field)
+            static void MinesSet(Pole[,] field, int xBound, int yBound)
             {
                 int mines = 0;
                 do
                 {
                     Random random = new Random();
-                    int x = random.Next(0, 10);
-                    int y = random.Next(0, 10);
+                    int x = random.Next(0, xBound);
+                    int y = random.Next(0, yBound);
                     if (field[x, y].value < 9)
                     {
                         field[x, y].value = 9;
 
-                        if (x < 9 && y < 9) field[x + 1, y + 1].value++;
+                        if (x < (xBound-1) && y < (yBound-1)) field[x + 1, y + 1].value++;
 
                         if (x > 0 && y > 0) field[x - 1, y - 1].value++;
-                        if (x < 9 && y > 0) field[x + 1, y - 1].value++;
+                        if (x < (xBound-1) && y > 0) field[x + 1, y - 1].value++;
 
-                        if (x > 0 && y < 9) field[x - 1, y + 1].value++;
+                        if (x > 0 && y < (yBound-1)) field[x - 1, y + 1].value++;
 
-                        if (x < 9) field[x + 1, y].value++;
-                        if (y < 9) field[x, y + 1].value++;
+                        if (x < (xBound-1)) field[x + 1, y].value++;
+                        if (y < (yBound-1)) field[x, y + 1].value++;
 
                         if (x > 0) field[x - 1, y].value++;
                         if (y > 0) field[x, y - 1].value++;
@@ -43,21 +43,21 @@ namespace SaperUnix
                 } while (mines < 10);
             }
 
-            static void ZeroSet(Pole[,] field)
+            static void ZeroSet(Pole[,] field, int xBound, int yBound)
             {
-                for (int i = 0; i < 10; i++)
-                    for (int j = 0; j < 10; j++)
+                for (int i = 0; i < xBound; i++)
+                    for (int j = 0; j < yBound; j++)
                     {
                         field[i, j].value = 0;
                         field[i, j].hide = false;
                     }
             }
 
-            static void Check9(Pole[,] field)
+            static void Check9(Pole[,] field, int xBound, int yBound)
             {
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i < xBound; i++)
                 {
-                    for (int j = 0; j < 10; j++)
+                    for (int j = 0; j < yBound; j++)
                     {
                         if (field[i, j].value > 9)
                             field[i, j].value = 9;
@@ -65,24 +65,27 @@ namespace SaperUnix
                 }
             }
 
-            static void Show(Pole[,] field)
+            static void Show(Pole[,] field, int xBound, int yBound)
             {
-            Console.Write("  ");
-                for (int i = 0; i < 10; i++)
-                Console.Write(i+" ");
-                Console.Write("\n");
+            Console.Write("   ");
+                for (int i = 0; i < yBound; i++)
+                if(i<10)Console.Write(i+"  ");
+                else Console.Write(i + " ");
+            Console.Write("\n");
 
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i < xBound; i++)
                 {
-                Console.Write(i + " ");
-                    for (int j = 0; j < 10; j++)
-                    {
-                        if (field[i, j].hide)
-                            Console.Write(field[i, j].value+" ");
-                        else
-                            Console.Write("# ");
-                    }
-                    Console.Write("\n");
+                    if(i<10) Console.Write(i + "  ");
+                    else Console.Write(i + " ");
+
+                    for (int j = 0; j < yBound; j++)
+                        {
+                            if (field[i, j].hide)
+                                Console.Write(field[i, j].value+"  ");
+                            else
+                                Console.Write("#  ");
+                        }
+                        Console.Write("\n");
                 }
             }
 
@@ -111,7 +114,6 @@ namespace SaperUnix
                 else
                     Console.WriteLine("Przegrana!!");
 
-
             }
 
             static bool CheckPlay(Pole[,] field, int w, int q)
@@ -125,7 +127,7 @@ namespace SaperUnix
                 }
 
             }
-            static int InputX(string message)
+            static int InputX(string message, int X)
             {
                 int x = 0;
                 do
@@ -147,11 +149,11 @@ namespace SaperUnix
                     Console.WriteLine("błedne dane wartosc powinna byc wieksza lub rowna od 0");
                         return 5000;
                     }
-                } while (x > 9);
+                } while (x > X);
 
                 return x;
             }
-            static bool Moves(Pole[,] field)
+            static bool Moves(Pole[,] field, int xBound, int yBound)
             {
                 int x = 0, y = 0;
                 do
@@ -168,59 +170,88 @@ namespace SaperUnix
                     Console.WriteLine("------------------------------------");
                     do
                     {
-                        x = InputX("x : ");
+                        x = InputX("x : ", xBound);
                     } while (x == 5000);
 
                 do
                 {
-                    y = InputX("y : ");
+                    y = InputX("y : ", yBound);
                 } while (x == 5000);
 
                 change(field, x, y);
                     Console.Clear();
-                    Show(field);
+                    Show(field, xBound, yBound);
 
                 } while (CheckPlay(field, x, y));
 
                 return CheckPlay(field, x, y);
             }
-            static void Saper(Pole[,] field)
+            static void Saper(/*Pole[,] field,*/ int xBound, int yBound)
             {
+                
                 bool game = true;
+                
+                    InitGame(ref xBound, ref yBound);
+                    Pole[,] field = new Pole[xBound, yBound];
+                    do
+                    {
+                        ZeroSet(field, xBound,yBound);
+                        MinesSet(field, xBound, yBound);
+                        Check9(field, xBound, yBound);
+                        Show(field, xBound, yBound);
+                        Moves(field, xBound, yBound);
+                        
+                        for (int i = 0; i < xBound; i++)
+                        {
+                            for (int j = 0; j < yBound; j++)
+                            {
+                                Console.Write(field[i, j].value);
+                                Console.Write("\t");
+                            }
+                            Console.Write("\n");
+                        }
+
+                        Console.Write("Gramy jeszcze raz czy koniec? [g/k]");
+                        string Again = Console.ReadLine();
+                        if (Again.Equals("k"))
+                            game = false;
+
+                    } while (game);
+
+            }
+            static void setBounds(ref int x, ref int y)
+            {
+                Console.WriteLine("Jakie wymiary chcesz ustawic musza byc mniejsz od 50");
+
                 do
                 {
-                    ZeroSet(field);
-                    MinesSet(field);
-                    Check9(field);
-                    Show(field);
-                    Moves(field);
+                    x = InputX("x : ", 50);
+                } while (x == 5000);
 
+                do
+                {
+                    y = InputX("y : ", 50);
+                } while (x == 5000);
+            }
 
+            static void InitGame(ref int x, ref int y)
+            {
+                Console.WriteLine("czy chcesz zmienic wymiary planszy ? [y/n]");
+                string Bounds = Console.ReadLine();
 
-                    for (int i = 0; i < 10; i++)
-                    {
-                        for (int j = 0; j < 10; j++)
-                        {
-                            Console.Write(field[i, j].value);
-                            Console.Write("\t");
-                        }
-                        Console.Write("\n");
-                    }
-
-                    Console.Write("Gramy jeszcze raz czy koniec? [g/k]");
-                    string Again = Console.ReadLine();
-                    if (Again.Equals("k"))
-                        game = false;
-
-                } while (game);
+                if (Bounds.Equals("y"))
+                    setBounds(ref x,ref y);
 
             }
 
             static void Main(string[] args)
             {
-                Pole[,] field = new Pole[10, 10];
+                int x = 10;
+                int y = 10;
+                //InitGame(ref x,ref y);
+                //Pole[,] field = new Pole[x, y];
 
-                Saper(field);
+                Saper(/*field,*/ x, y);
                 Console.ReadKey();
             }
         }
